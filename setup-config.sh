1626f2844ee1614a8328ec172d2a54586c34a21b
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2012-06-11 22:02:48 +0200

    Disable mail stuff

--- setup-config.sh
+++ setup-config.sh
@@ -6,18 +6,18 @@ ln -s -f $HERE/.bashrc ~/.bashrc
 ln -s -f $HERE/.gitconfig ~/.gitconfig
 ln -s -f $HERE/.gitignore ~/.gitignore
 ln -s -f $HERE/.tmux.conf ~/.tmux.conf
-ln -s -f $HERE/.mailcap ~/.mailcap
 ln -s -f $HERE/.vimrc ~/.vimrc
 
-mkdir -p ~/.mutt/cache/bodies
-mkdir -p ~/.mutt/cache/headers
-mkdir -p ~/.mutt/temp
+# ln -s -f $HERE/.mailcap ~/.mailcap
+# mkdir -p ~/.mutt/cache/bodies
+# mkdir -p ~/.mutt/cache/headers
+# mkdir -p ~/.mutt/temp
 
-ln -s -f $HERE/.mutt/fzk.muttrc ~/.mutt/fzk.muttrc
-ln -s -f $HERE/.mutt/gmail.muttrc ~/.mutt/gmail.muttrc
-ln -s -f $HERE/.mutt/signature ~/.mutt/signature
-ln -s -f $HERE/.mutt/aliases ~/.mutt/aliases
+# ln -s -f $HERE/.mutt/fzk.muttrc ~/.mutt/fzk.muttrc
+# ln -s -f $HERE/.mutt/gmail.muttrc ~/.mutt/gmail.muttrc
+# ln -s -f $HERE/.mutt/signature ~/.mutt/signature
+# ln -s -f $HERE/.mutt/aliases ~/.mutt/aliases
 
-mkdir -p ~/.offlineimap
-ln -s -f $HERE/offlineimap.py ~/.offlineimap/offlineimap.py
-ln -s -f $HERE/.offlineimaprc ~/.offlineimaprc
+# mkdir -p ~/.offlineimap
+# ln -s -f $HERE/offlineimap.py ~/.offlineimap/offlineimap.py
+# ln -s -f $HERE/.offlineimaprc ~/.offlineimaprc
