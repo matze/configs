310f431600ad8a82b5c269de355abdef4a42c0cb
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-02-06 22:43:47 +0100

    Install autojump from source

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 HERE=`pwd`
 VUNDLE_URL="https://github.com/gmarik/vundle.git"
+VUNDLE_DIR="$HOME/.vim/bundle/vundle"
+
+AUTOJUMP_URL="https://github.com/joelthelion/autojump.git"
+AUTOJUMP_DIR="$HOME/.autojump"
+
 
 # Create necessary directories
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
@@ -16,4 +21,14 @@ ln -s -f $HERE/.vimrc ~/.vimrc
 ln -s -f $HERE/lilypink.vim ~/.vim/colors/lilypink.vim
 
 # Checkout Vundle
-git clone $VUNDLE_URL  ~/.vim/bundle/vundle
+if [ ! -d "$VUNDLE_DIR" ]; then
+    git clone $VUNDLE_URL $VUNDLE_DIR
+fi
+
+# Install autojump
+if [ ! -d "$AUTOJUMP_DIR" ]; then
+    cd /tmp
+    git clone $AUTOJUMP_URL
+    cd /tmp/autojump
+    /tmp/autojump/install.sh --local
+fi
