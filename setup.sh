55a35f05458aeffe3908022ae3bbd262d86993c9
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-02-06 23:13:52 +0100

    Import ranger configuration

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ VUNDLE_DIR="$HOME/.vim/bundle/vundle"
 AUTOJUMP_URL="https://github.com/joelthelion/autojump.git"
 AUTOJUMP_DIR="$HOME/.autojump"
 
-
 # Create necessary directories
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
+mkdir -p ~/.config
 
 # Setup symlinks
 ln -s -f $HERE/.bashrc ~/.bashrc
@@ -19,6 +19,8 @@ ln -s -f $HERE/.tmux.conf ~/.tmux.conf
 ln -s -f $HERE/.Xdefaults ~/.Xdefaults
 ln -s -f $HERE/.vimrc ~/.vimrc
 ln -s -f $HERE/lilypink.vim ~/.vim/colors/lilypink.vim
+
+ln -s -f $HERE/ranger ~/.config/ranger
 
 # Checkout Vundle
 if [ ! -d "$VUNDLE_DIR" ]; then
