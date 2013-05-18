9a761f2bbd6b9c276b6fcc73af292d85254a6165
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-05-18 12:34:57 +0200

    Remove config for unused apps

--- setup.sh
+++ setup.sh
@@ -19,8 +19,6 @@ ln -s -f $HERE/.tmux.conf ~/.tmux.conf
 ln -s -f $HERE/.Xdefaults ~/.Xdefaults
 ln -s -f $HERE/.vimrc ~/.vimrc
 ln -s -f $HERE/ranger ~/.config/ranger
-ln -s -f $HERE/newsbeuter/urls ~/.newsbeuter/urls
-ln -s -f $HERE/newsbeuter/config ~/.newsbeuter/config
 
 for source in `find vim/ -type f`; do
     ln -s -f $PWD/$source ~/.$source
@@ -29,12 +27,4 @@ done
 # Checkout Vundle
 if [ ! -d "$VUNDLE_DIR" ]; then
     git clone $VUNDLE_URL $VUNDLE_DIR
-fi
-
-# Install autojump
-if [ ! -d "$AUTOJUMP_DIR" ]; then
-    cd /tmp
-    git clone $AUTOJUMP_URL
-    cd /tmp/autojump
-    /tmp/autojump/install.sh --local
 fi
