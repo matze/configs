87f86a8633b96f7c5c6fda867a8c7bf24942cd6b
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-09-29 21:19:16 +0200

    Add nm-applet to awesome startup

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -379,3 +379,4 @@ client.add_signal("unfocus", function(c) c.border_color = beautiful.border_norma
 
 awful.util.spawn_with_shell("gnome-settings-daemon")
 awful.util.spawn_with_shell("gnome-keyring-daemon")
+awful.util.spawn_with_shell("nm-applet")
