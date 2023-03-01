24cbcef092de1289b212661baa66d29bc6b00bce
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2023-03-01 13:15:49 +0100

    nvim/lualine: truncate branch name and sparkle diff

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,34 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    require('lualine').setup {
+    require("lualine").setup {
       options = {
-        theme = 'kanagawa',
+        theme = "kanagawa",
       },
       sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'filetype'},
-        lualine_y = {'diff'},
-        lualine_z = {'location'}
+        lualine_a = {"mode"},
+        lualine_b = {
+          {
+            "branch",
+            fmt = function(str)
+              if #str >= 30 then
+                return str:sub(1, 29) .. "…"
+              else
+                return str
+              end
+            end
+          }
+        },
+        lualine_c = {"filename"},
+        lualine_x = {"encoding"},
+        lualine_y = {
+          "filetype",
+          {
+            "diff",
+            symbols = { added = " ", modified = "柳", removed = " " }
+          },
+        },
+        lualine_z = {"location"}
       },
     }
   end,
