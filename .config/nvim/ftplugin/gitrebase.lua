c39bc9a91b489bc1817cb8b465ce5c408336cf9c
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2023-03-08 11:55:40 +0100

    nvim: fix gitrebase ftplugin

--- .config/nvim/ftplugin/gitrebase.lua
+++ .config/nvim/ftplugin/gitrebase.lua
@@ -1,11 +1,11 @@
 function cycle_action()
-  line = vim.api.nvim_get_current_line()
-  char = string.sub(line, 1, 1)
+  local line = vim.api.nvim_get_current_line()
+  local char = string.sub(line, 1, 1)
 
   if #char == 1 then
-    transitions = {p = "fixup", f = "edit", e = "squash", s = "pick"}
-    space_pos = string.find(line, " ")
-    next = transitions[char]
+    local transitions = {p = "fixup", f = "edit", e = "squash", s = "pick"}
+    local space_pos = string.find(line, " ")
+    local next = transitions[char]
 
     if next ~= nil then
       vim.api.nvim_set_current_line(next .. string.sub(line, space_pos))
