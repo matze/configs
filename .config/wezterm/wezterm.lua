0303c947acf4f20419b8c6b0009efd9bb62d7086
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2024-03-09 18:13:54 +0100

    wez: add fullscreen toggle keybind

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,5 +7,11 @@ config.enable_tab_bar = false
 config.use_resize_increments = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.warn_about_missing_glyphs = false
+config.keys = {
+  {
+    key = 'F11',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
 
 return config
