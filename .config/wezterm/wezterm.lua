d1e0a2af3d536e6a90b5349927f3ce6af85a0064
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2024-03-09 17:43:51 +0100

    Add wezterm configuration

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font_size = 14.5
+config.color_scheme = 'Rasi (terminal.sexy)'
+config.enable_tab_bar = false
+config.use_resize_increments = true
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.warn_about_missing_glyphs = false
+
+return config
