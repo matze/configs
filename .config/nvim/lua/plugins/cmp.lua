70ecdc09d974fd7a64b39c3d4fa331b4f964b220
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2024-05-25 08:56:40 +0200

    nvim: drop snippet from cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -8,11 +8,6 @@ return {
       local cmp = require("cmp")
 
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "nvim_lsp_signature_help" },
@@ -30,8 +25,6 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lsp-signature-help",
       "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
     }
   },
   {
