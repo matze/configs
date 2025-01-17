0f6bebd43c186c76e120d89e12a67ab8719ac673
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-01-17 21:20:17 +0100

    nvim: farewell telescope, welcome snacks.picker

---
+++ .config/nvim/lua/plugins/picker.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        prompt = " ",
+        ui_select = true,
+        win = {
+          input = {
+            keys = {
+              ["<Esc>"] = "close",
+              ["<C-c>"] = { "close", mode = "i" },
+            }
+          },
+        },
+      },
+    },
+    keys = {
+      { "<C-p>", function() Snacks.picker.files() end, desc = "Find Files" },
+      { "<C-f>", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+      { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    },
+    dependencies = {
+      {
+        "pkazmier/zk-nvim",
+        branch = "snacks-picker",
+        config = function()
+          require("zk").setup({
+            picker = "snacks_picker",
+          })
+        end
+      }
+    },
+  }
+}
