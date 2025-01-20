d0589ac383ada55aebf2792aad3f569d52a99cbd
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-01-20 17:22:42 +0100

    nvim/picker: replicate old behavior

--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -17,7 +17,8 @@ return {
     },
     keys = {
       { "<C-p>", function() Snacks.picker.files() end, desc = "Find Files" },
-      { "<C-f>", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<C-g>", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<C-f>", function() Snacks.picker.grep_word() end, desc = "Grep" },
       { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
       { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
       { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
