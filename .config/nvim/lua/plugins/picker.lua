91f539f5182125b7d80a2f232818a093f36614b0
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-01-20 17:25:45 +0100

    nvim/picker: and also diagnostics

--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -17,11 +17,12 @@ return {
     },
     keys = {
       { "<C-p>", function() Snacks.picker.files() end, desc = "Find Files" },
-      { "<C-g>", function() Snacks.picker.grep() end, desc = "Grep" },
-      { "<C-f>", function() Snacks.picker.grep_word() end, desc = "Grep" },
+      { "<C-g>", function() Snacks.picker.grep() end, desc = "Live grep" },
+      { "<C-f>", function() Snacks.picker.grep_word() end, desc = "Grep word" },
       { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
       { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
       { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "ge", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
     },
     dependencies = {
       {
