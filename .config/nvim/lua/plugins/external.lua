20b1a2d857db32aa1bf8bdba077c2fd34ac89e34
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-01-27 15:13:14 +0100

    nvim/zk: bind C-b to backlinks

--- .config/nvim/lua/plugins/external.lua
+++ .config/nvim/lua/plugins/external.lua
@@ -13,6 +13,7 @@ return {
     keys = {
       { "<C-n>", "<cmd>:ZkNotes<CR>", remap = false },
       { "<C-t>", "<cmd>:ZkTags<CR>", remap = false },
+      { "<C-b>", "<cmd>:ZkBacklinks<CR>", remap = false },
       { "<Leader>zc", "<cmd>:ZkNew<CR>", remap = false },
       { "<Leader>zn", ":'<,'>ZkNewFromTitleSelection<CR>", mode = "v", remap = false },
     },
