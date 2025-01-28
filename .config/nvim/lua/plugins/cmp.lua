17b1b8713f001a8305443b2890144a926b01a3f4
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-01-28 23:54:45 +0100

    nvim: drop AI shit and update dependencies

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -6,7 +6,6 @@ return {
     version = "*",
     dependencies = {
       "moyiz/blink-emoji.nvim",
-      "giuxtaposition/blink-cmp-copilot",
     },
     opts = {
       keymap = {
@@ -18,28 +17,13 @@ return {
         nerd_font_variant = "mono",
       },
       sources = {
-        default = { "buffer", "lsp", "path", "snippets", "copilot", },
+        default = { "buffer", "lsp", "path", "snippets", },
         providers = {
           emoji = {
             module = "blink-emoji",
             name = "Emoji",
             score_offset = 15, -- the higher the number, the higher the priority
             opts = { insert = true }, -- Insert emoji (default) or complete its name
-          },
-          copilot = {
-            module = "blink-cmp-copilot",
-            name = "copilot",
-            score_offset = 100,
-            async = true,
-            transform_items = function(_, items)
-              local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-              local kind_idx = #CompletionItemKind + 1
-              CompletionItemKind[kind_idx] = "Copilot"
-              for _, item in ipairs(items) do
-                item.kind = kind_idx
-              end
-              return items
-            end,
           },
         }
       },
