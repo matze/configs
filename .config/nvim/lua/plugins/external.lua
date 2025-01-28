0d1700b0d7b75e9d8a7d0fa8401a5ea3c4dbdafc
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-01-28 23:55:43 +0100

    nvim: move zk stuff to picker

--- .config/nvim/lua/plugins/external.lua
+++ .config/nvim/lua/plugins/external.lua
@@ -8,19 +8,4 @@ return {
       require("wastebin").setup()
     end,
   },
-  {
-    "mickael-menu/zk-nvim",
-    keys = {
-      { "<C-n>", "<cmd>:ZkNotes<CR>", remap = false },
-      { "<C-t>", "<cmd>:ZkTags<CR>", remap = false },
-      { "<C-b>", "<cmd>:ZkBacklinks<CR>", remap = false },
-      { "<Leader>zc", "<cmd>:ZkNew<CR>", remap = false },
-      { "<Leader>zn", ":'<,'>ZkNewFromTitleSelection<CR>", mode = "v", remap = false },
-    },
-    config = function()
-      require("zk").setup({
-        picker = "telescope",
-      })
-    end
-  }
 }
--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -28,6 +28,13 @@ return {
       {
         "pkazmier/zk-nvim",
         branch = "snacks-picker",
+        keys = {
+          { "<C-n>", "<cmd>:ZkNotes<CR>", remap = false },
+          { "<C-t>", "<cmd>:ZkTags<CR>", remap = false },
+          { "<C-b>", "<cmd>:ZkBacklinks<CR>", remap = false },
+          { "<Leader>zc", "<cmd>:ZkNew<CR>", remap = false },
+          { "<Leader>zn", ":'<,'>ZkNewFromTitleSelection<CR>", mode = "v", remap = false },
+        },
         config = function()
           require("zk").setup({
             picker = "snacks_picker",
