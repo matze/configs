f22a8eba83cda750ed5f0d0d2973a1ebde2e784d
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-05-25 13:35:17 +0200

    nvim: add copilot again

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("codecompanion").setup({
+        strategies = {
+          chat = {
+            adapter = "copilot",
+          },
+          inline = {
+            adapter = "copilot",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    enabled = false,
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+        copilot_node_command = "node",
+      })
+    end,
+  },
+}
