314e71fbfbf8af8416179dfed0863361d2e2a59a
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-11-28 11:13:22 +0100

    nvim: use different zk mappings

--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -32,9 +32,9 @@ return {
       {
         "zk-org/zk-nvim",
         keys = {
-          { "<C-n>", "<cmd>:ZkNotes<CR>", remap = false },
-          { "<C-t>", "<cmd>:ZkTags<CR>", remap = false },
-          { "<C-b>", "<cmd>:ZkBacklinks<CR>", remap = false },
+          { "<Leader>zo", "<cmd>:ZkNotes<CR>", remap = false },
+          { "<Leader>zt", "<cmd>:ZkTags<CR>", remap = false },
+          { "<Leader>zb", "<cmd>:ZkBacklinks<CR>", remap = false },
           { "<Leader>zc", "<cmd>:ZkNew<CR>", remap = false },
           { "<Leader>zn", ":'<,'>ZkNewFromTitleSelection<CR>", mode = "v", remap = false },
         },
