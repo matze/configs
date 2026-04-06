7de58eb0bf1ddbd02625167540985f283bfba971
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2026-04-06 14:15:12 +0200

    nvim/blink: enable ghost text

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -441,6 +441,9 @@ require('blink.cmp').setup({
     use_nvim_cmp_as_default = true,
     nerd_font_variant = "mono",
   },
+  completion = {
+    ghost_text = { enabled = true },
+  },
   sources = {
     default = { "buffer", "lsp", "path" },
   },
